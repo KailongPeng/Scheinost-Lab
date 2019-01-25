bb31ba5291dba62815d2619790116f91f09cf51c
kailong peng <kailong@cuda5.med.yale.internal>
2019-01-25 14:48:56 -0500

    .

--- cpm_trial.m
+++ cpm_trial.m
@@ -16,13 +16,15 @@ pathname = '/mnt/store1/mridata2/mri_group/HCP_data/HCP_900_DATA/EMOTION_LR/matr
 
 
 %cd(pathname);
+clear a;
 a = dir([pathname '*_GSR_*']);
 fileList = kailong_extractfield(a,'name');
+clear a;
 id = load('/mnt/store4/mri_group/siyuan_data/HCP515/all_id.mat');
 id = id.all_id;
-for curr_id = 1:size(id,1) % find the filenameID that is among the id list
-    Index{curr_id} = find(~cellfun(@isempty,strfind(fileList, num2str(id(curr_id)))));
-end
+% for curr_id = 1:size(id,1) % find the filenameID that is among the id list
+%     Index{curr_id} = find(~cellfun(@isempty,strfind(fileList, num2str(id(curr_id)))));
+% end
 for curr_id = 1:size(id,1) % find the filenameID that is among the id list
     Index{curr_id} = find(contains(fileList,num2str(id(curr_id)))==1);
 end
